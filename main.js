cd576f3aa9b2bcbd4c04564858ba81d8ee09d929
aarush24 <82024065+aarush24@users.noreply.github.com>
2022-02-05 09:26:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+Webcam.attach('#camera');
+
+camera=document.getElementById("camera");
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+function take_snapshot()
+{
+    Wencam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+
+    });
+}
+
+console.log('ml5 version:',ml5.version);
+
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/train/image')
+
+function modelLoaded()
+{
+    console.log('Model Loaded');
+}
